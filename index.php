ddc5be496757ddb37a9d2aa0cef12e5cf672b4b7
Andres Cayambe <andrescayambe99@hotmail.com>
2023-05-18 19:05:40 +0200

    [first-commit]print-hotel

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php Hotel</title>
+</head>
+<body>
+    <ul>
+        <?php foreach ($hotels as $hotel): ?>
+            <li>
+                <ul>
+                    <li>Nome:<?php echo $hotel['name']?></li>
+                    <li>description:<?php echo $hotel['description']?></li>
+                    <li>parking:<?php echo $hotel['parking']?></li>
+                    <li>vote:<?php echo $hotel['vote']?></li>
+                    <li>distance_to_center:<?php echo $hotel['distance_to_center']?></li>
+                </ul>
+            </li>
+        <?php endforeach; ?>
+        <div>
+          <?php
+
+          ?>
+        </div>
+    </ul>
+</body>
+</html>
